85300da7418a69161d1b06285dd16d328e2371c8
hanju <ahj1903@naver.com>
2023-01-05 14:31:20 +0900

    Bean find

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -17,6 +17,7 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService(){
+
         return new MemberServiceImpl(memberRepository());
     }
 
@@ -27,6 +28,7 @@ public class AppConfig {
 
     @Bean
     public OrderService orderService(){
+
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
